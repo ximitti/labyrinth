5db24c7b818f3b8d9e94097d8fad0922689c7317
Alex Schmitt <alex.schmitt@outlook.com>
2020-12-17 20:20:16 -0300

    Primeira versão funcional, falta condição de vitória e reset

--- script.js
+++ script.js
@@ -21,28 +21,28 @@ const generateBoard = () => {
 
   for (let i = 0; i < map.length; i++) {
     const line = document.createElement("div");
-    line.className = "line";
+    line.classList.add("line");
     let charArr = map[i].split("");
     for (let j = 0; j < charArr.length; j++) {
       const block = document.createElement("div");
       switch (charArr[j]) {
         case "W":
-          block.className = "wall";
+          block.classList.add("wall");
           line.appendChild(block);
           break;
         case " ":
-          block.className = "path";
+          block.classList.add("path");
           line.appendChild(block);
           break;
         case "S":
           const player = document.createElement("div");
           player.id = "player";
           block.appendChild(player);
-          block.className = "start";
+          block.classList.add("start");
           line.appendChild(block);
           break;
         case "F":
-          block.className = "finish";
+          block.classList.add("finish");
           line.appendChild(block);
           break;
         default:
@@ -52,18 +52,85 @@ const generateBoard = () => {
     board.appendChild(line);
   }
 };
+
+const moveUp = () => {
+  // linha de cima
+  let up = player.parentElement.parentElement.previousSibling.children;
+  // pegar o índice atual
+  let iul = 0;
+  let arrNextLine = player.parentElement.parentElement.children;
+  for (let i = 0; i < arrNextLine.length; i++) {
+    if (arrNextLine[i].childElementCount > 0) {
+      iul = i;
+    }
+  }
+  if (up[iul].className === "path") {
+    up[iul].appendChild(player);
+  }
+};
+
+const moveDown = () => {
+  // linha de baixo
+  let down = player.parentElement.parentElement.nextSibling.children;
+  // pegar o índice atual
+  let idl = 0;
+  let arrDownLine = player.parentElement.parentElement.children;
+  for (let i = 0; i < arrDownLine.length; i++) {
+    if (arrDownLine[i].childElementCount > 0) {
+      idl = i;
+    }
+  }
+  if (down[idl].className === "path") {
+    down[idl].appendChild(player);
+  }
+};
+
+const moveLeft = () => {
+  let left = player.parentElement.previousSibling;
+  if (left.className === "path" || left.className === "start") {
+    left.appendChild(player);
+  }
+};
+
+const moveRight = () => {
+  let right = player.parentElement.nextSibling;
+  if (right.className === "path" || right.className === "finish") {
+    right.appendChild(player);
+  }
+};
+
+const movePlayer = (key) => {
+  switch (key) {
+    case "ArrowUp":
+      moveUp();
+      break;
+
+    case "ArrowDown":
+      moveDown();
+      break;
+
+    case "ArrowLeft":
+      moveLeft();
+      break;
+
+    case "ArrowRight":
+      moveRight();
+      break;
+
+    default:
+  }
+};
+
+const resetGame = () => {
+  const board = document.querySelector("#board");
+  board.innerHTML = '';
+  generateBoard();
+}
+
+// Start Game
 generateBoard();
 
-const startPath = document.querySelector(".start");
-const endPath = document.querySelector(".finish");
-
 document.addEventListener("keydown", (event) => {
-  const keyName = event.key;
-  console.log("keydown event\n\n" + "key: " + keyName);
-
-
-  // capturar o parente da div do player
-  // para movimentos laterais, capturar o irmão (sibling) e verificar se não é
-  // muro
-  // para movimentos verticais, capturar o pai do pai, capturar o irmão (próximo ou anterior), descer para o filho e verificar se é muro ou não
+  const keyPressed = event.key;
+  movePlayer(keyPressed);
 });
